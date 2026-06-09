61545505e8c3a81753d5fd101b83cf1bae376f46
Chris Knapp <chrisknappcc@gmail.com>
2026-06-09 12:52:09 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@clerk/clerk-react'],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          clerk: ['@clerk/clerk-react'],
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
