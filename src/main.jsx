438546fba2dc5aa3fe82b30770d58aa3766e4699
Chris Knapp <chrisknappcc@gmail.com>
2026-06-10 09:40:35 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,10 @@ if (!PUBLISHABLE_KEY) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} clerkJSVersion="5.124">
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    clerkJSUrl="https://grown-goldfish-4.clerk.accounts.dev/npm/@clerk/clerk-js@5.124.0/dist/clerk.browser.js"
+  >
     <App />
   </ClerkProvider>
 )
