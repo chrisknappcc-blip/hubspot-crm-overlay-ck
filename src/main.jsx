3bd462e04ad64bd0f7ea135cb200ae33daf7db3c
Chris Knapp <chrisknappcc@gmail.com>
2026-06-10 09:32:33 -0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} clerkJSVersion="5.124">
     <App />
   </ClerkProvider>
 )
